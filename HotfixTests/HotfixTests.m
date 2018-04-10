98d3a0c178d4a46e5db4d5a062a1911db36c1d16
xuemiyang <xuemiyang@qq.com>
2018-04-10 17:36:08 +0800

    新增测试代码

--- HotfixTests/HotfixTests.m
+++ HotfixTests/HotfixTests.m
@@ -7,6 +7,60 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Hotfix.h"
+
+@interface MyObject : NSObject
+- (NSString *)isBoolWithCount:(int)count;
+- (void)fixMethod;
+- (NSString *)twoArgMethod:(NSInteger)arg1 arg2:(CGFloat)arg2;
+- (NSString *)threeArgMethod:(NSInteger)arg1 arg2:(CGFloat)arg2 arg3:(CGFloat)arg3;
+@property (nonatomic, assign) NSInteger count;
+@end
+
+@implementation MyObject
+- (instancetype)init {
+    if (self = [super init]) {
+        _count = 102;
+    }
+    return self;
+}
+- (NSString *)isBoolWithCount:(int)count {
+    return [NSString stringWithFormat:@"sss%d", count];
+}
+- (void)fixMethod {
+    NSLog(@"ssss");
+}
+- (NSString *)twoArgMethod:(NSInteger)arg1 arg2:(CGFloat)arg2 {
+    return [NSString stringWithFormat:@"arg1=%d, arg2=%f", (int)arg1, arg2];
+}
+- (NSString *)threeArgMethod:(NSInteger)arg1 arg2:(CGFloat)arg2 arg3:(CGFloat)arg3; {
+    return [NSString stringWithFormat:@"arg1=%d, arg2=%f, arg3=%f", (int)arg1, arg2, arg3];
+}
+@end
+
+@interface MySubObject : MyObject
+@property (nonatomic, strong) MyObject *obj;
++ (CGFloat)getHeight;
++ (NSInteger)getIntegerWithCount:(NSInteger)count;
+@end
+
+@implementation MySubObject
+- (instancetype)init {
+    if (self = [super init]) {
+        _obj = [[MyObject alloc] init];
+    }
+    return self;
+}
+
++ (CGFloat)getHeight {
+    return 77.9;
+}
+
++ (NSInteger)getIntegerWithCount:(NSInteger)count {
+    return 80 * count;
+}
+
+@end
 
 @interface HotfixTests : XCTestCase
 
@@ -27,6 +81,11 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSString *js = @"fixMethod('MySubObject', 'fixMethod', false, 1, function(instance, originInvocation, originArguments) {console.log('bbbb'); callInvocation(originInvocation); var result = callInstanceMethod(instance, 'isBoolWithCount:', [20]); console.log(result); var count = callInstanceMethod(instance, 'count'); console.log(count); var obj = callInstanceMethod(instance, 'obj'); console.log(obj); callInstanceMethod(obj, 'setCount:', [888]); count = callInstanceMethod(obj, 'count'); console.log(count); var url = callClassMethod('NSURL', 'URLWithString:', ['http://www.baidu.com']); console.log(url); var scheme = callInstanceMethod(url, 'scheme'); console.log(scheme); var height = callClassMethod('MySubObject', 'getHeight'); console.log(height); var str = callInstanceMethod(instance, 'twoArgMethod:arg2:', [87, 13.45]); console.log(str); str = callInstanceMethod(instance, 'threeArgMethod:arg2:arg3:', [99, 15.5, 88.23]); console.log(str); })";
+    evalString(js);
+    MySubObject *obj = [[MySubObject alloc] init];
+    [obj fixMethod];
+    NSLog(@"end");
 }
 
 - (void)testPerformanceExample {
